5f14581e964b812cc0840b5300ce3e36414ffec0
Joseph <joseph.nijo.2001@gmail.com>
2023-06-28 05:38:29 +0930

    ranking working

--- mindameld-trivia/src/components/Ranks.js
+++ mindameld-trivia/src/components/Ranks.js
@@ -1,63 +1,39 @@
 import React, { useEffect, useState } from "react";
 import "./Ranks.css";
 
-export default function Ranks({ }) {
+export default function Ranks() {
+  const [leaderBoard, setLeaderBoard] = useState([]);
 
-  const [leaderBoard, setLeaderBoard] = useState([])
-  // useEffect(() => {
-  //   const leaderBoardData = await fetchScoreData()
-  // })
-  // const fetchScoreData = async () => {
-  //   const res = await fetch('https://649a246479fbe9bcf8404f40.mockapi.io/api/highscores', {
-  //     method: 'GET',
-  //     headers: { 'content-type': 'application/json' },
-  //   })
-  //   const data = await res.json()
-  //   setLeaderBoard(data)
-  //   console.log(leaderBoard)
-  // }
   useEffect(() => {
     fetch('https://649a246479fbe9bcf8404f40.mockapi.io/api/highscores', {
       method: 'GET',
       headers: { 'content-type': 'application/json' },
     })
       .then((data) => data.json())
-      .then(data => setLeaderBoard(data))
-    console.log(leaderBoard)
+      .then(data => setLeaderBoard(data));
+  }, []);
 
-  }, [])
-  leaderBoard.map(data => {
-    console.log(data)
-  })
+  const sortedLeaderBoard = leaderBoard.sort((a, b) => b.score - a.score).slice(0, 10);
+
   return (
-    <div
-      className="text-center"
-      style={{
-        margin: "50px 0px 0px 0px",
-      }}
-    >
+    <div className="text-center" style={{ margin: "50px 0px 0px 0px" }}>
       <h3>Top 10 Ranks</h3>
 
       <div style={{ margin: "25px 0 0 0" }}>
-        <div
-          style={{
-            margin: "0 0 0 13%",
-            width: "85%",
-            height: "35px",
-          }}
-        >
-          <div className="rank-title ">Rank</div>
+        <div style={{ margin: "0 0 0 13%", width: "85%", height: "35px" }}>
+          <div className="rank-title">Rank</div>
           <div className="rank-title rank-title2">Score</div>
         </div>
         <ol className="list-group list-group-numbered">
-          {
-            leaderBoard.map((score) => {
-              return <li className="list-group-item d-flex justify-content-between align-items-start no-border">
-                <div className="ms-2 me-auto">{score["loggedInUser"]}</div>
-                <span className="badge rounded-pill">{score["score"]}</span>
-              </li>
-            })}
-
+          {sortedLeaderBoard.map((score, index) => (
+            <li
+              key={index}
+              className="list-group-item d-flex justify-content-between align-items-start no-border"
+            >
+              <div className="ms-2 me-auto">{score.loggedInUser}</div>
+              <span className="badge rounded-pill">{score.score}</span>
+            </li>
+          ))}
         </ol>
       </div>
     </div>
